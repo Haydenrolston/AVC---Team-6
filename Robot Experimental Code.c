3dcba31be99e0c6f1dbca709884b0cf42cc5eef9
Startrekker007 <keshavhraj@gmail.com>
2017-05-04 10:33:54 +1200

    Coded up methods to read the camera and position of white strip
    Much of the details of this update is in the comments in the code, but note: this code hasn't been tested or compiled yet as I was on my laptop and wrote it up on github in the vichub. It should if its working  correctly find where in the image the white strip is by defining endpoints yo.

---
+++ Robot Experimental Code.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include "E101.h"
+//APOLOGIEs FOR MY SHODDY CODE IN ADVANCE LOL
+int main()
+{
+	
+	
+	return null;
+}
+
+int[][] getCameraLine(int height, int[][][] inpImage)
+{
+	
+	int[][][] image = new int[240][420][3];
+	image = inpImage;
+	int linePixels = new int[420][3];
+	for(int i = 0; i<420; i++)
+	{
+		linePixels[i][0] = image[height][i][0];
+		linePixels[i][1] = image[height][i][1];
+		linePixels[i][2] = image[height][i][2];
+	}
+	return linePixels;
+}
+int[][][] readCamera()
+{
+	//Cant remember E101.h api for take picture
+	//Assuming I can put the colour values into a 3d array 420/240 res containing 3 values for colour
+	int[][][] image = new int[240][420][3];
+}
+int[] getLinePosition(int colorTolerance, int[] calibratedColorBW, int[][] line)
+{
+	//Not sure if this way is gonna work but basically:
+	//it takes the average of 5 pixel values reading from the left to the right of the image and sees whether
+	//the average is within the tolerance of the calibrated white value, if it is then it will record that 
+	//column in the line as the start position and wont record more start  until it encounters an average value near the calibrated
+	//black value where it will record the white end point, will write a seperate method to generate commands for robot motion based
+	//on the position of the white section in the image. I haven't tested this to work yet as I wrote this up on my laptop directly on github
+	//if you see errors, please please please correct them, corrections are most welcome and bring us closer to our goal of getting
+	//our roguebot to the end of the first quadrant
+	int[] whiteEndPts = new int[2];
+	bool inWhite = false;
+	for(int i = 0; i<line.length;i++)
+	{
+		
+		int avgColor = 0;
+		for(int j = 0; j<5;j++)
+		{
+			avgColor = avgColor+getAvgColor(line[i]);
+			i++;
+		}
+		avgColor= avgColor/5;
+		if(avgColor < (calibratedColorBW[1]+colorTolerance) && avgColor > (calibratedColorBW[1]-colorTolerance))
+		{
+			if(inWhite==false){inWhite=true;
+			whiteEndPts[0] = i;
+			}
+			
+		}
+		else if(avgColor < (calibratedColorBW[0]+colorTolerance) && avgColor>(calibratedColorBW[0]-colorTolerance))
+		{
+			if(inWhite==true){inWhite=false; whiteEndPts[1]=i;}
+		}
+	}
+	return whiteEndPts;
+}
+int getAvgColor(int[] RGB)
+{
+	int avgColor = 0;
+	avgColor = avgColor+RGB[0];
+	avgColor = avgColor+RGB[1];
+	avgColor = avgColor+RGB[2];
+	avgColor = avgColor/3;
+	return avgColor;
+	
+}
+int[] calibrateCamera(int[][][] inpImage)
+{
+	int averageColorW = 0;
+	int averageColorB = 0;
+	int sumW = 0;
+	int sumB = 0;
+	image = inpImage;
+	for(int i = 0; i<image.length; i++)
+	{	
+		int avgRGB = 0;
+		avgRGB = avgRGB+inpImage[i][1][0];
+		avgRGB = avgRGB+inpImage[i][1][1];
+		avgRGB = avgRGB+inpImage[i][1][2];
+		avgRGB = avgRGB/3;
+		averageColorB = averageColorB+avgRGB;
+	}
+	for(int i = 0; i<image.length; i++)
+	{
+		int avgRGB = 0;
+		avgRGB = avgRGB+inpImage[i][209][0];
+		avgRGB = avgRGB+inpImage[i][209][1];
+		avgRGB = avgRGB+inpImage[i][209][2];
+		avgRGB = avgRGB/3;
+		averageColorW = averageColorW+avgRGB;
+	}
+	averageColorW = averageColorW/240;
+	averageColorB = averageColorB/240;
+	int[] calibratedBW = {averageColorB, averageColorW};
+	return calibratedBW;
+}
+void driveStraight(int speed, int timeS, int timeMS)
+{
+	return null;
+}
+void turn(int diff, int timeS, int timeMS)
+{
+	return null;
+}
