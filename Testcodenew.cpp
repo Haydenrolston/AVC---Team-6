6c93a2898f25ab3025b6979c85fe01245e0a568a
Startrekker007 <keshavhraj@gmail.com>
2017-05-12 08:36:50 +1200

    More code

--- Testcodenew.cpp
+++ Testcodenew.cpp
@@ -10,16 +10,50 @@ int* getCameraLine()
     {
         line[i] = get_pixel(i,j,3);
     }
-    
+    return line;
     
 
 }
-int* getWhites(int** line)
+int* getWhites(int* line)
 {
-
+      int* boolLine = new int[320];
+      for(int i = 0; i<320; i++)
+      {
+            if(line[i]=>BWThreshold){boolLine[i]=1;}else
+                if(line[i]<BWThreshold){boolLine[i]=0;}
+      }
+    return boolLine;
 }
 
+int* getLoc(int* line)
+{
+      int startPos;
+      int endPos;
+    bool inWhite = false;
+    int* loc = new int[2];
+    for(int i = 0; i<320; i++)
+    {
+           if(line[i]==1 && inWhite==false){inWhite=true; startPos=i;}
+           if(line[i]==0 && inWhite=true){endPos=i; break;}
+        
+    }
+    loc[0]=startPos;
+    loc[1] = endPos;
+    return loc;
+}
 
+int getTurnDiff(int* loc)
+{
+    int diff = 0;
+    diff=(loc[0]+loc[1])/2;
+    diff=diff-160;
+    return diff;
+}
+
+void drive(int diff, int timeS, int timeMS)
+{
+       
+}
 
 
 
